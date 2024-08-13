3ba13ba28cab5c425051b72992dc12c8e0032407
Jorge Matos <jvsmatos1@gmail.com>
2024-08-13 23:46:52 +0100

    adjusts to navigation + first steps of main section (starting w/ hero section)

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@
             menuBtn2.className = "fa-solid fa-bars";
         }
     }
+
+/* ----- SHADOW ON NAVIGATION WHEN SCROLLING ----- */
+    window.onscroll = function(){
+        headerShadow();
+    }
+
+    function headerShadow(){
+        const navHeader = document.getElementById("header");
+
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+
+            navHeader.style.boxShadow   = "0 2px 6px rgba(0, 0, 0, 0.2)";
+            navHeader.style.height      = "70px";
+            navHeader.style.lineHeight  = "70px";
+        }else{
+            navHeader.style.boxShadow   = "none";
+            navHeader.style.height      = "90px";
+            navHeader.style.lineHeight  = "90px";
+        }
+    }
