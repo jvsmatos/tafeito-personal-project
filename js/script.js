77d21457d1686f479035a68b84ba65a5d2a086e2
jvsmatos <jvsmatos1@gmail.com>
2024-08-27 16:03:49 +0100

    price simulation JS logic: - setting min and max price per type of service - setting fixed prices for extra services - recalculating estimated prices for every changes - show/hide fields - validating fields
    there is still a few more adjusts to be done, like organization and add second button for direct contact or not

--- js/script.js
+++ js/script.js
@@ -113,3 +113,174 @@
     tabClientes.addEventListener('click', mostrarClientes);
     tabProf.addEventListener('click', mostrarProfissionais);
 
+
+/* ----- PRICE SIMULATOR ----- */
+    // Valores mínimos e máximos para cálculo
+    const valoresServicos = {
+        'pintura-interna': { valorMinimo: 9, valorMaximo: 13, valorMinimoServico: 150 },
+        'pintura-externa': { valorMinimo: 12, valorMaximo: 18, valorMinimoServico: 250 },
+        'pequenos-retoques': { valorMinimo: 13, valorMaximo: 16, valorMinimoServico: 100 }
+    };
+
+    // Preços fixos para serviços adicionais
+    const precosAdicionais = {
+        teto: 170,
+        rodape: 100,
+        esquadrias: 100,
+        papel: 200,
+        reparo: 200,
+        isolamento: 150,
+        andaimes: 3000
+    };
+
+    // Referências aos elementos do DOM
+    const servicoField      = document.getElementById('servico');
+    const areaField         = document.getElementById('area');
+    const estadoField       = document.getElementById('estado');
+    const acabamentoField   = document.getElementById('acabamento');
+    const inicioField       = document.getElementById('inicio');
+    const totalField        = document.getElementById('total');
+    const campos            = document.getElementById('campos');
+    const extras            = document.getElementById('extras');
+    const totalSection      = document.querySelector('.total');
+    const botaoPedido       = document.getElementById('botaoPedido');
+
+    // Função para calcular o total estimado
+    function calcularTotal() {
+        const area              = parseFloat(areaField.value) || 0;
+        const estadoValue       = parseFloat(estadoField.value) || 1;
+        const acabamentoValue   = parseFloat(acabamentoField.value) || 1;
+        const inicioValue       = parseFloat(inicioField.value) || 1;
+
+        // Identificar o tipo de serviço selecionado
+        const tipoServico   = servicoField.value;
+        const valores       = valoresServicos[tipoServico] || valoresServicos.pinturaInterna;
+
+        let precoMinimo = valores.valorMinimo * area;
+        let precoMaximo = valores.valorMaximo * area;
+
+        precoMinimo *= estadoValue * acabamentoValue * inicioValue;
+        precoMaximo *= estadoValue * acabamentoValue * inicioValue;
+
+        // Aplicar valor entre o calculado e o mínimo para nunca ficar menor que o mínimo
+        precoMinimo = Math.max(precoMinimo, valores.valorMinimoServico);
+        precoMaximo = Math.max(precoMaximo, valores.valorMinimoServico);
+
+        // Acrescentar valores extras para cada item do looping que é verificado com checked
+        const selectedExtras = document.querySelectorAll('.input-check:checked');
+        selectedExtras.forEach(extra => {
+            const key = extra.id; // Obter o id do checkbox, que corresponde à chave do preço fixo
+            precoMinimo += precosAdicionais[key] || 0;
+            precoMaximo += precosAdicionais[key] || 0;
+        });
+
+        totalField.textContent = `€${precoMinimo.toFixed(2)} - €${precoMaximo.toFixed(2)}`;
+    }
+
+    // Função para exibir todos os campos após a seleção do serviço
+    function showFields() {
+        campos.style.display = 'flex';
+        extras.style.display = 'flex';
+        totalSection.style.display = 'block';
+    }
+
+    function hideFields(){
+        campos.style.display = 'none';
+        extras.style.display = 'none';
+        totalSection.style.display = 'none';
+    }
+
+    // Função para validar campos e aplicar classes
+    function validarCampos() {
+        let allFieldsValid = true;
+
+        function validarCampo(campo, valid) {
+            if (valid) {
+                campo.classList.add('ok');
+                campo.classList.remove('error');
+            } else {
+                campo.classList.add('error');
+                campo.classList.remove('ok');
+                allFieldsValid = false;
+            }
+        }
+
+        // Validar todos os campos obrigatórios
+        
+        validarCampo(servicoField, servicoField.value !== '');
+        validarCampo(areaField, areaField.value !== '' && !isNaN(areaField.value) && parseFloat(areaField.value) > 0);
+        validarCampo(estadoField, estadoField.value !== '');
+        validarCampo(acabamentoField, acabamentoField.value !== '');
+        validarCampo(inicioField, inicioField.value !== '');
+
+        return allFieldsValid;
+    }
+
+    // Função para criar o pedido
+    function createOrder() {
+        // Validar campos
+        const camposValidos = validarCampos();
+
+        if (camposValidos) {
+            alert('Pedido submetido com sucesso!');
+        } else {
+            alert('Por favor, preencha todos os campos corretamente.');
+        }
+    }
+
+    // Adicionar event listeners para atualizar o total e verificar os campos
+    const camposParaCalculo = [
+        areaField,
+        estadoField,
+        acabamentoField,
+        inicioField
+    ];
+
+    camposParaCalculo.forEach(campo => {
+        campo.addEventListener('input', () => {
+            calcularTotal();
+        });
+    });
+
+    // Event listener para mostrar campos e recalcular total ao mudar o serviço
+    servicoField.addEventListener('change', () => {
+        
+        if (servicoField.value === "0") { // Se a opção "Selecione o serviço" for escolhida
+            hideFields();
+        } else {
+            showFields();
+            resetForm();
+            //calcularTotal(); // Recalcular total quando o serviço muda
+        }
+    });
+
+    // Event listener para recalcular total ao selecionar opções adicionais
+    document.querySelectorAll('.input-check').forEach(check => {
+        check.addEventListener('change', () => {
+            calcularTotal();
+        });
+    });
+
+    // Função para resetar o formulário ao trocar de serviço
+    function resetForm() {
+        // Limpar todos os campos de input
+        areaField.value = '';
+        estadoField.value = '';
+        acabamentoField.value = '';
+        inicioField.value = '';
+
+        // Desmarcar todas as checkboxes dentro do elemento 'extras'
+        const checkboxesExtras = extras.querySelectorAll('.input-check');
+        checkboxesExtras.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+
+        // Limpar total estimado
+        totalField.textContent = `€0.00 - €0.00`;
+
+        // Remover classes de validação
+        document.querySelectorAll('.ok, .error').forEach(campo => {
+            campo.classList.remove('ok', 'error');
+        });
+    }
+
